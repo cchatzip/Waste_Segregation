63f59836fcdc37e4e4fbaebfb4db0ddb2379f643
cchatzip <christodoulos.hadjipetrou98@gmail.com>
2024-07-31 19:11:21 +0300

    -Model metrics and Main page configured for streamlit app

---
+++ pages/1_📈_Model_Metrics.py
@@ -0,0 +1,119 @@
+import streamlit as st
+import time
+import numpy as np
+
+import Helper
+import params
+
+st.set_page_config(page_title="Model Metrics", page_icon="📈")
+
+st.markdown("# Model Metrics")
+st.sidebar.header("Model Metrics")
+
+source_radio = st.sidebar.radio(
+    "Select an Option", params.METRICS_LIST)
+st.write(
+    """This page is dedicated on showing the metrics of the model to give you a grasp of it's performance"""
+)
+
+#IF THE OPTION IS CONFUSION MATRIX
+if source_radio == params.CONFUSION_MATRIX:
+    col1, col2 = st.columns(2)
+
+
+    with col1:
+        try:
+            st.image(params.CONF_MTRX, caption="Confusion Matrix",
+                use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
+
+    with col2:
+        try:
+            st.image(params.CONF_MTRX_NORM, caption="Confusion Matrix Normalized",
+                use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
+
+
+#IF THE OPTION IS CURVES
+if source_radio == params.CURVES:
+    try:
+        st.image(params.F1_CURVE, use_column_width=True)
+    except Exception as ex:
+        st.error("Error occurred while opening the image.")
+        st.error(ex)
+    
+    try:
+        st.image(params.P_CURVE, use_column_width=True)
+    except Exception as ex:
+        st.error("Error occurred while opening the image.")
+        st.error(ex)
+    
+    try:
+        st.image(params.PR_CURVE, use_column_width=True)
+    except Exception as ex:
+        st.error("Error occurred while opening the image.")
+        st.error(ex)
+    
+    try:
+        st.image(params.R_CURVE, use_column_width=True)
+    except Exception as ex:
+        st.error("Error occurred while opening the image.")
+        st.error(ex)
+
+#IF THE OPTION IS LABELS
+if source_radio == params.LABELS:
+    try:
+        st.image(params.LBLS, use_column_width=True)
+    except Exception as ex:
+        st.error("Error occurred while opening the image.")
+        st.error(ex)
+
+#IF THE OPTION IS Loss & MAP Plots
+if source_radio == params.RESULTS:
+    try:
+        st.image(params.RSLTS, use_column_width=True)
+    except Exception as ex:
+        st.error("Error occurred while opening the image.")
+        st.error(ex)
+
+#IF THE OPTION IS Loss & MAP Plots
+if source_radio == params.VALIDATION:
+    col1, col2 = st.columns(2)
+
+
+    with col1:
+        try:
+            st.image(params.BATCH0_LBLS, caption="Batch 0 labels",
+                use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
+
+    with col2:
+        try:
+            st.image(params.BATCH0_PRED, caption="Batch 0 model predictions",
+                use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
+    
+    #Page second row
+    with col1:
+        try:
+            st.image(params.BATCH1_LBLS, caption="Batch 1 labels",
+                use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
+
+    with col2:
+        try:
+            st.image(params.BATCH1_PRED, caption="Batch 1 model predictions",
+                use_column_width=True)
+        except Exception as ex:
+            st.error("Error occurred while opening the image.")
+            st.error(ex)
--- params.py
+++ params.py
@@ -8,13 +8,38 @@ DETECTION_MODEL = os.path.join(MODEL_DIR, 'best.pt')
 IMAGE = 'Image'
 WEBCAM = 'Webcam'
 
+# Model Metrics
+CONFUSION_MATRIX = 'Confusion Matrix'
+CURVES = 'Curves'
+LABELS = 'Labels'
+RESULTS = 'Loss & MAP Plots'
+VALIDATION = 'Validation batches and Detections'
+
 SOURCES_LIST = [IMAGE, WEBCAM]
+METRICS_LIST = [CONFUSION_MATRIX, CURVES, LABELS, RESULTS, VALIDATION]
 
 
 # Images config
 IMAGES_DIR = os.path.join('.', 'Test_Images')
 DEFAULT_IMAGE = os.path.join(IMAGES_DIR, 'PET_bottles.jpg')
 DEFAULT_DETECT_IMAGE = os.path.join(IMAGES_DIR, 'PET_bottles_detected.jpg')
+
+#Metrics Images config
+METRICS_DIR = os.path.join('.', 'runs', 'detect', 'train2_300ep')
+
+CONF_MTRX = os.path.join(METRICS_DIR, 'confusion_matrix.png')
+CONF_MTRX_NORM = os.path.join(METRICS_DIR, 'confusion_matrix_normalized.png')
+F1_CURVE = os.path.join(METRICS_DIR, 'F1_curve.png')
+P_CURVE = os.path.join(METRICS_DIR, 'P_curve.png')
+PR_CURVE = os.path.join(METRICS_DIR, 'PR_curve.png')
+R_CURVE = os.path.join(METRICS_DIR, 'R_curve.png')
+LBLS = os.path.join(METRICS_DIR, 'labels.jpg')
+RSLTS = os.path.join(METRICS_DIR, 'results.png')
+BATCH0_LBLS = os.path.join(METRICS_DIR, 'val_batch0_labels.jpg')
+BATCH0_PRED = os.path.join(METRICS_DIR, 'val_batch0_pred.jpg')
+BATCH1_LBLS = os.path.join(METRICS_DIR, 'val_batch1_labels.jpg')
+BATCH1_PRED = os.path.join(METRICS_DIR, 'val_batch1_pred.jpg')
+
 
 # Webcam
 WEBCAM_PATH = 0
